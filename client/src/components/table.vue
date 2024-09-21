3a944413ee79849f1c9cb7250ea19eb86ea85b2b
David Moore <mlexperiencedavid@gmail.com>
2024-09-21 18:35:52 +0100

    integrating a table

---
+++ client/src/components/table.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-table :data="users">
+    <thead slot="head">
+        <th>Name</th>
+        <th>Age</th>
+        <th>Email</th>
+        <th>Address</th>
+    </thead>
+    <tbody slot="body" slot-scope="{displayData}">
+        <tr v-for="row in displayData" :key="row.id">
+          <td>{{ row.Name }}</td>
+          <td>{{ row.Age }}</td>
+          <td>{{ row.Email }}</td>
+          <td>
+              {{ row.Address}}
+          </td>
+        </tr>
+    </tbody>
+  </v-table>
+</template>
+
+
+<script>
+import users from './users.json'
+
+export default {
+  name: 'TableView',
+  data: () => ({
+    users
+  })
+}
+</script>
+
+
