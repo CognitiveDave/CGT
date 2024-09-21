88a968a75e3a026f32f81749245308704b7c35ee
David Moore <mlexperiencedavid@gmail.com>
2024-09-21 22:32:41 +0100

    styling

--- client/src/components/table.vue
+++ client/src/components/table.vue
@@ -1,27 +1,42 @@
 <template>
-  <div>
-  <label>Filter by Age:</label>
-  <input v-model="filters.name.value"/>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4"> it</h1>
+      <p class="lead">Do your  ere</p>
+      <hr class="my-4">
+      <p>All in one place </p>
+    </div>
+    <div class="row"></div>
+    <div class="row">
+    <div class="col-sm">   
+    </div>
+    <div class="col-lg">
+      <label>Filters</label>
+      <input v-model="filters.name.value"/>
 
-  <v-table :data="users"
-          :filters="filters">
-    <thead slot="head">
-        <th>Name</th>
-        <th>Age</th>
-        <th>Email</th>
-        <th>Address</th>
-    </thead>
-    <tbody slot="body" slot-scope="{displayData}">
-        <tr v-for="row in displayData" :key="row.id">
-          <td>{{ row.Name }}</td>
-          <td>{{ row.Age }}</td>
-          <td>{{ row.Email }}</td>
-          <td>
-              {{ row.Address}}
-          </td>
-        </tr>
-    </tbody>
-  </v-table>
+      <v-table :data="users"
+              :filters="filters">
+        <thead slot="head">
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Address</th>
+        </thead>
+        <tbody slot="body" slot-scope="{displayData}">
+            <tr v-for="row in displayData" :key="row.id">
+              <td>{{ row.Name }}</td>
+              <td>{{ row.Age }}</td>
+              <td>{{ row.Email }}</td>
+              <td>
+                  {{ row.Address}}
+              </td>
+            </tr>
+        </tbody>
+      </v-table>
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
   </div>
 </template>
 
@@ -34,7 +49,7 @@ export default {
   data: () => ({
     users,
     filters: {
-      name: { value: '', keys: ['Age'] }
+      name: { value: '', keys: ['Email'] }
     }
   })
 }
--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import SmartTable from 'vuejs-smart-table'
+import '@popperjs/core/dist/umd/popper';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 
 Vue.use(SmartTable)
 
